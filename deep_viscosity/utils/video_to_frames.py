e31222de112b9ebef91b6fcfe6230432d2684b00
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-09 08:53:39 +0200

    docs: add docstrings to methods & raise VideoCaptureError

--- deep_viscosity/utils/video_to_frames.py
+++ deep_viscosity/utils/video_to_frames.py
@@ -4,8 +4,18 @@ import torch
 from torchvision import transforms
 
 
+class VideoCaptureError(Exception):
+    pass
+
+
 class VideoToFrames:
     def __init__(self, data_path: str, output_folder: str) -> None:
+        """Initializes the VideoToFrames class.
+
+        Args:
+            data_path (str): Path to the directory containing the video files.
+            output_folder (str): Path to the directory where the frames will be saved.
+        """
         self._data_path = data_path
         self._output_folder = output_folder
         self._jpg_folder = os.path.join(self._output_folder, "jpg")
@@ -16,6 +26,10 @@ class VideoToFrames:
     ### Public methods ###
 
     def process_videos_in_directory(self) -> None:
+        """
+        Processes all the video files in the directory.
+        Saves the frames as jpg files and the frames as tensors.
+        """
         video_files = [
             file for file in os.listdir(self._data_path) if file.endswith(".avi")
         ]
@@ -25,23 +39,22 @@ class VideoToFrames:
 
     ### Private methods ###
 
-    def _create_folder(self, _output_folder: str) -> None:
-        if not os.path.exists(_output_folder):
-            os.makedirs(_output_folder)
-
     def _video_to_frames(self, video_path: str) -> None:
+        """Extracts frames from a video file and saves them as jpg files and as tensors.
+
+        Args:
+            video_path (str): Path to the video file.
+        """
         vidcap = cv2.VideoCapture(video_path)
+        if not self._check_vidcap(vidcap):
+            return
+
         video_name = os.path.splitext(os.path.basename(video_path))[0]
         video_jpg_folder = os.path.join(self._jpg_folder, video_name)
         video_tensor_path = os.path.join(self._tensor_folder, f"{video_name}.pt")
         self._create_folder(video_jpg_folder)
 
-        if not vidcap.isOpened():
-            print(f"Error: Could not open video {video_path}")
-            return
-
-        if not os.path.exists(video_jpg_folder):
-            os.makedirs(video_jpg_folder)
+        self._create_folder(video_jpg_folder)
 
         frame_count = 0
         frames_list = []
@@ -66,6 +79,32 @@ class VideoToFrames:
         frames_tensor = torch.stack(frames_list)
         torch.save(frames_tensor, video_tensor_path)
         print(f"Done! Extracted {frame_count} frames to {video_jpg_folder}")
+
+    def _create_folder(self, _output_folder: str) -> None:
+        """Creates a folder if it does not aldready exist.
+
+        Args:
+            _output_folder (str): Path to the folder that will be created.
+        """
+
+        if not os.path.exists(_output_folder):
+            os.makedirs(_output_folder)
+
+    def _check_vidcap(self, vidcap: cv2.VideoCapture) -> bool:
+        """Checks if a video capture object is opened.
+
+        Args:
+            vidcap (cv2.VideoCapture): A video capture object.
+
+        Returns:
+            bool: True if the video capture object is opened.
+
+        Raises:
+            VideoCaptureError: If the video capture object is not opened.
+        """
+        if not vidcap.isOpened():
+            raise VideoCaptureError("Error opening video file.")
+        return True
 
 
 def main():
