12e966325210c4f7c4a510e1eb5886a912db8530
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-10 11:48:18 +0200

    feat: add dataloader, greyscale image and frame selection

--- deep_viscosity/main.py
+++ deep_viscosity/main.py
@@ -6,11 +6,16 @@ from dataset import Dataset
 
 
 def main():
-    processed_data_path = os.path.join("data", "processed")
-    tensor_data_path = os.path.join(processed_data_path, "tensor")
+    tensor_data_path = os.path.join("data", "processed", "tensor")
 
-    dataset = Dataset(tensor_data_path, 32)
-    dataset.create_dataloaders()
+    dataset = Dataset(tensor_data_path, 10)
+    train_loader, val_loader, test_loader = dataset.create_dataloaders()
+
+    # Iterate through DataLoader
+    for batch_idx, (features, labels) in enumerate(train_loader):
+        print(f"Batch {batch_idx + 1}")
+        print("Features:", features)
+        print("Labels:", labels)
 
 
 if __name__ == "__main__":
