ff8f9fa7681ce1b6fbe8b2247789abe46b833b76
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-10 13:38:27 +0200

    chore: remove not needed code

--- deep_viscosity/main.py
+++ deep_viscosity/main.py
@@ -1,18 +1,5 @@
-import os
-from dataset import Dataset
-
-
 def main():
-    tensor_data_path = os.path.join("data", "processed", "tensor")
-
-    dataset = Dataset(tensor_data_path, 10)
-    train_loader, val_loader, test_loader = dataset.create_dataloaders()
-
-    # Iterate through DataLoader
-    for batch_idx, (features, labels) in enumerate(train_loader):
-        print(f"Batch {batch_idx + 1}")
-        print("Features:", features)
-        print("Labels:", labels)
+    return
 
 
 if __name__ == "__main__":
