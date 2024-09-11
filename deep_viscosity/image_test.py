22cfede522307aa81bad56cf64d980b9d7180c05
hannamlmv <hanna.malmvall@gmail.com>
2024-09-11 14:38:56 +0200

    feat: added different transformations

--- deep_viscosity/image_test.py
+++ deep_viscosity/image_test.py
@@ -1,23 +1,39 @@
 from torchvision import transforms
 from PIL import Image
+import numpy as np
+import cv2
 
-# Open the original image
+# Original image
 image_original = Image.open('data/processed/jpg/1.00_1/frame127.jpg')
-
-# Show the undedited image 
 image_original.show()
 
-# Define a transform to normalize the image
+# Normalized image
 transform = transforms.Compose([
   transforms.ToTensor(), 
   transforms.Normalize(mean=[0.5], std=[0.5])
   ])
+image_tensor_norm = transform(image_original)
+image_normalized = transforms.ToPILImage()(image_tensor_norm)
+image_normalized.show()
 
-# Apply the transform to the image
-image_tensor = transform(image_original)
+# Equalized image
+image_np = np.array(image_original)
+image_equalized_np = cv2.equalizeHist(image_np)
+image_equalized = Image.fromarray(image_equalized_np)
+image_equalized.show()
 
-# Convert tensor back to PIL images for display
-image_transformed = transforms.ToPILImage()(image_tensor)
 
-# Display the transformed image
-image_transformed.show()
+
+
+### JUST FOR FUN; these suck ###
+
+# Equalized then normalized
+image_tensor_eqnorm = transform(image_equalized)
+image_eqnorm = transforms.ToPILImage()(image_tensor_eqnorm)
+image_eqnorm.show()
+
+# Normalized then equalized
+image_np_norm = np.array(image_normalized)
+image_normeq_np = cv2.equalizeHist(image_np_norm)
+image_normeq = Image.fromarray(image_normeq_np)
+image_normeq.show()
