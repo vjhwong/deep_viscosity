3216da42b381d461bf77695febea1663b1bd33de
hannamlmv <hanna.malmvall@gmail.com>
2024-09-12 16:37:58 +0200

    feat: added model

---
+++ deep_viscosity/utils/functions.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def conv3d_output_size(img_size, padding, kernel_size, stride):
+    # compute output shape of conv3D
+    outshape = (
+        np.floor(
+            (img_size[0] + 2 * padding[0] - (kernel_size[0] - 1) - 1) / stride[0] + 1
+        ).astype(int),
+        np.floor(
+            (img_size[1] + 2 * padding[1] - (kernel_size[1] - 1) - 1) / stride[1] + 1
+        ).astype(int),
+        np.floor(
+            (img_size[2] + 2 * padding[2] - (kernel_size[2] - 1) - 1) / stride[2] + 1
+        ).astype(int),
+    )
+    return outshape
