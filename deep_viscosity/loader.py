ea73bb029a817fadc0e4638f6ebbd9bbed3038f6
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-12 15:57:53 +0200

    docs: add doc-string to function

--- deep_viscosity/loader.py
+++ deep_viscosity/loader.py
@@ -11,30 +11,43 @@ from dataset import DeepViscosityDataset
 from utils.transforms import transform
 
 
-def create_reg_datasets(batch_size: int, processed_data_path: str, validation_size: float = 0.2, test_size: float = 0.2) -> Tuple[DataLoader, DataLoader, DataLoader]:
+def create_reg_datasets(
+    batch_size: int,
+    processed_data_path: str,
+    validation_size: float = 0.2,
+    test_size: float = 0.2,
+) -> Tuple[DataLoader, DataLoader, DataLoader]:
     """Create the training, testing and validation data loaders for training
     regression models. Note that the each training video is stored inside a
     folder with the folder named with its corresponding viscosity value.
 
     Args:
-        batch_size: batch size used for training the model
-        processed_data_path: path to processed folder
+        batch_size (int): batch size used for training the model
+        processed_data_path (str): path to processed folder
+        validation_size (float, optional): validation size. Defaults to 0.2.
+        test_size (float, optional): test size. Defaults to 0.2.
     """
     x_list = []
     y_list = []
 
     for filename in os.listdir(processed_data_path):
-        viscosity = re.search(r'^[\d\.]+(?=_)', filename)
+        viscosity = re.search(r"^[\d\.]+(?=_)", filename)
         if viscosity:
             viscosity = viscosity.group()
             x_list.append(viscosity)
             y_list.append(float(viscosity))
 
     # train, test split
-    train_list, test_list, train_label, test_label = train_test_split(x_list, y_list, test_size=test_size, random_state=0)
+    train_list, test_list, train_label, test_label = train_test_split(
+        x_list, y_list, test_size=test_size, random_state=0
+    )
     transform_function = transform()
-    train_set = DeepViscosityDataset(processed_data_path, train_list, train_label, transform=transform_function)
-    test_set = DeepViscosityDataset(processed_data_path, test_list, test_label, transform=transform_function)
+    train_set = DeepViscosityDataset(
+        processed_data_path, train_list, train_label, transform=transform_function
+    )
+    test_set = DeepViscosityDataset(
+        processed_data_path, test_list, test_label, transform=transform_function
+    )
 
     # split into training and validation batches
     num_train = len(train_set)
@@ -46,11 +59,12 @@ def create_reg_datasets(batch_size: int, processed_data_path: str, validation_si
     valid_sampler = SubsetRandomSampler(valid_idx)
 
     # loading train, validation and test data
-    train_loader = DataLoader(train_set, batch_size=batch_size, sampler=train_sampler, num_workers=0)
-    valid_loader = DataLoader(train_set, batch_size=batch_size, sampler=valid_sampler, num_workers=0)
+    train_loader = DataLoader(
+        train_set, batch_size=batch_size, sampler=train_sampler, num_workers=0
+    )
+    valid_loader = DataLoader(
+        train_set, batch_size=batch_size, sampler=valid_sampler, num_workers=0
+    )
     test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=0)
 
     return train_loader, test_loader, valid_loader
-
-
-train_loader, test_loader, valid_loader = create_reg_datasets(1, "data/processed")
