91b2e8d0e62d1f53db50b24811ea3e866d8cf6d8
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-13 09:21:52 +0200

    refactor: add lr and n_epochs as parameter to train

--- deep_viscosity/main.py
+++ deep_viscosity/main.py
@@ -3,6 +3,7 @@ from loader import create_reg_datasets
 from modelling.model import CNN3DVisco
 from modelling.train import train
 
+
 def main():
     processed_data_path = os.path.join("data", "processed")
     train_loader, test_loader, valid_loader = create_reg_datasets(
@@ -11,8 +12,7 @@ def main():
 
     model = CNN3DVisco(20, 640, 480)
 
-    train(model, train_loader)
-    
+    train(model, train_loader, learning_rate=0.01, num_epochs=3)
     return
 
 
