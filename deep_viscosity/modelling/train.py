a3eca95813bbc9e4d86db20dde0a80e4fe51468e
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-13 09:21:28 +0200

    chore: fix imports and doc-strings

--- deep_viscosity/modelling/train.py
+++ deep_viscosity/modelling/train.py
@@ -1,35 +1,40 @@
 import torch
+import torch.utils.data
 import torch.nn as nn
 import torch.optim as optim
 from modelling.model import CNN3DVisco
+from tqdm import tqdm
 
 
-def train(model: nn.Module, data_loader: torch.utils.data.DataLoader) -> None:
-    # Set device, standard is to use GPU if available
+def train(
+    model: nn.Module,
+    data_loader: torch.utils.data.DataLoader,
+    learning_rate: float,
+    num_epochs: int,
+) -> None:
+    """Train the model using the given data loader and hyperparameters.
+
+    Args:
+        model (nn.Module): The model to train.
+        data_loader (torch.utils.data.DataLoader): The data loader to use for training.
+        learning_rate (float): The learning rate to use for training.
+        num_epochs (int): The number of epochs to train for.
+    """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # Set hyperparameters, godtyckliga värden
-    learning_rate = 0.001
-    num_epochs = 10
-
-    # Define loss function and optimizer
-    # change criterion to least squares loss
+    model.to(device)
 
     criterion = nn.MSELoss()
     optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
 
-    # Set the model to training mode
     model.train()
 
-    # Training loop
-    for epoch in range(num_epochs):
+    for epoch in tqdm(range(num_epochs)):
         for batch_idx, (inputs, targets) in enumerate(data_loader):
             inputs = inputs.to(device)
             targets = targets.to(device)
 
             # Forward pass
             outputs = model(inputs)
-            loss = criterion(outputs, targets.float())
             loss = criterion(outputs, targets)
 
             # Backward pass and optimization
@@ -38,10 +43,8 @@ def train(model: nn.Module, data_loader: torch.utils.data.DataLoader) -> None:
             optimizer.step()
 
             # Print loss every 10 batches
-            if (batch_idx + 1) % 10 == 0:
-                print(
-                    f"Epoch [{epoch+1}/{num_epochs}], Batch [{batch_idx+1}/{len(data_loader)}], Loss: {loss.item()}"
-                )
+            # if (batch_idx + 1) % 10 == 0:
+        # print(f"Batch [{batch_idx+1}/{len(data_loader)}], Loss: {loss.item()}")
 
     # Save the trained model
     torch.save(model.state_dict(), "trained_model.pth")
