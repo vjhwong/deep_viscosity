019b08709243b1dc7267f45d036b21cc4c774c9a
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-23 09:04:25 +0200

    chore: reformat code - PEP8

--- deep_viscosity/preprocessing/frame_selection.py
+++ deep_viscosity/preprocessing/frame_selection.py
@@ -1,23 +1,28 @@
 import os
 import shutil
 
+
 def frame_selection(output_path: str, input_path: str, frame_range: tuple):
-  if not os.path.exists(output_path):
-    os.makedirs(output_path)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
-  for video in os.listdir(input_path):
-    os.makedirs(os.path.join(output_path, video))
-    for frame in os.listdir(os.path.join(input_path, video)):
-       index = int(frame.split("_")[2].split(".")[0])
-       if index >= frame_range[0] and index <= frame_range[1]:
-          shutil.copy(os.path.join(input_path, video, frame), os.path.join(output_path, video, frame))
+    for video in os.listdir(input_path):
+        os.makedirs(os.path.join(output_path, video))
+        for frame in os.listdir(os.path.join(input_path, video)):
+            index = int(frame.split("_")[2].split(".")[0])
+            if index >= frame_range[0] and index <= frame_range[1]:
+                shutil.copy(
+                    os.path.join(input_path, video, frame),
+                    os.path.join(output_path, video, frame),
+                )
 
-  
+
 def main():
     output_path = "data/processed"
     input_path = "data/masked"
-    frame_range = (45,99)
+    frame_range = (45, 99)
     frame_selection(output_path, input_path, frame_range)
+
 
 if __name__ == "__main__":
     main()
