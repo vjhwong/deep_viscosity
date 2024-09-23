92ebabf556a96e7332121f4baafcb4ea5e145e77
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-23 12:19:04 +0200

    refactor: update train.py for DeepViscosity project
    - Add wandb initialization for experiment tracking
    - Log train and validation loss during training
    - Plot training and validation loss over epochs

--- deep_viscosity/modelling/train.py
+++ deep_viscosity/modelling/train.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 from modelling.model import CNN3DVisco
 from tqdm import tqdm
+import wandb
 import matplotlib.pyplot as plt
 
 
@@ -22,14 +23,23 @@ def train(
         learning_rate (float): The learning rate to use for training.
         num_epochs (int): The number of epochs to train for.
     """
+    wandb.init(
+        project="DeepViscosity",
+        config={
+            "learning_rate": learning_rate,
+            "architecture": "CNN",
+            "dataset": "DeepViscosity",
+            "epochs": num_epochs,
+        },
+    )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
     criterion = nn.MSELoss()
     optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
-    
+
     train_loss_values = []
-    val_loss_values= []
+    val_loss_values = []
 
     model.train()
 
@@ -46,34 +56,34 @@ def train(
             optimizer.zero_grad()
             train_loss.backward()
             optimizer.step()
-    
-        #here starts the code for the validation
+
+        # here starts the code for the validation
         train_loss /= len(train_loader)
         train_loss_values.append(train_loss)
 
         val_loss = 0.0
-      
+
         with torch.no_grad():
-      
+
             for batch_idx, (val_inputs, val_targets) in val_loader:
                 val_inputs = val_inputs.to(device)
                 val_targets = val_targets.to(device)
-              
+
                 val_outputs = model(val_inputs)
                 val_loss = criterion(val_outputs, val_targets.float()).item()
-      
+
         val_loss /= len(val_loader)
         val_loss_values.append(val_loss)
-      
+        wandb.log({"train_loss": train_loss, "val_loss": val_loss})
+    print(
+        f"Epoch [{epoch+1}/{num_epochs}], Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}"
+    )
 
-
-    print(f"Epoch [{epoch+1}/{num_epochs}], Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}")
-    
-    plt.plot(train_loss_values, label='Training Loss')
-    plt.plot(val_loss_values, label='Validation Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Training and Validation Loss over Epochs')
+    plt.plot(train_loss_values, label="Training Loss")
+    plt.plot(val_loss_values, label="Validation Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Training and Validation Loss over Epochs")
     plt.legend()
     plt.show()
 
