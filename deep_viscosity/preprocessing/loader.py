be9f9d6484664dad8c8547c1a4b70eba11e5a913
vjhwong <victor.ju.hin.wong@gmail.com>
2024-09-23 10:22:21 +0200

    feat: add stratified and data leakage prevention

--- deep_viscosity/preprocessing/loader.py
+++ deep_viscosity/preprocessing/loader.py
@@ -1,6 +1,5 @@
 import os
 from typing import Tuple
-from _typeshed import StrPath
 
 import pandas as pd
 from torch.utils.data.dataloader import DataLoader
@@ -13,64 +12,76 @@ from preprocessing.utils.enums import ViscosityClass
 
 def create_dataloaders(
     batch_size: int,
-    processed_data: StrPath,
+    processed_data_path: str,
     validation_size: float = 0.2,
-    test_size: float = 0.2,
+    test_size: float = 0.1,
 ) -> Tuple[DataLoader, DataLoader, DataLoader]:
     """Create the training, testing and validation data loaders for training
     regression models.
 
     Args:
         batch_size (int): batch size used for training the model
-        processed_data (StrPath): path to processed folder
+        processed_data_path (str): path to processed folder
         validation_size (float, optional): validation size. Defaults to 0.2.
         test_size (float, optional): test size. Defaults to 0.2.
     """
-    viscosity_df = pd.DataFrame(columns=["folder_name", "classification"])
+    # Initialize an empty list to store the data
+    viscosity_data = []
+    unique_viscosity_values = []
 
     # folder_name structure: [viscosity value]_[video number]
-    for folder_name in os.listdir(processed_data):
+    for folder_name in os.listdir(processed_data_path):
         viscosity = float(folder_name.split("_")[0])
-        viscosity_df = viscosity_df.append(
+        data = {"viscosity": viscosity, "classification": get_classification(viscosity)}
+        if data not in unique_viscosity_values:
+            unique_viscosity_values.append(data)
+        viscosity_data.append(
             {
                 "folder_name": folder_name,
-                "classification": get_classification(viscosity),
-            },
-            ignore_index=True,
+                "viscosity": viscosity,
+            }
         )
-
+    viscosity_df = pd.DataFrame(viscosity_data)
+    unique_viscosity_df = pd.DataFrame(unique_viscosity_values)
     split_test = StratifiedShuffleSplit(
         n_splits=1, test_size=test_size, random_state=42
     )
     for train_val_index, test_index in split_test.split(
-        viscosity_df, viscosity_df["classification"]
+        unique_viscosity_df, unique_viscosity_df["classification"]
     ):
-        X_train_val = viscosity_df.loc[train_val_index]
-        X_test = viscosity_df.loc[test_index]
+        train_val_viscosities = unique_viscosity_df.loc[train_val_index]
+        test_viscosities = unique_viscosity_df.loc[test_index]
 
     split_val = StratifiedShuffleSplit(
         n_splits=1, test_size=validation_size, random_state=42
     )
+    train_val_viscosities = train_val_viscosities.reset_index(drop=True)
     for train_index, val_index in split_val.split(
-        X_train_val, X_train_val["classification"]
+        train_val_viscosities, train_val_viscosities["classification"]
     ):
-        X_train = X_train_val.loc[train_index]
-        X_val = X_train_val.loc[val_index]
+        train_viscosities = train_val_viscosities.loc[train_index]
+        val_viscosities = train_val_viscosities.loc[val_index]
 
-    train_folders = X_train["folder_name"].tolist()
-    val_folders = X_val["folder_name"].tolist()
-    test_folders = X_test["folder_name"].tolist()
+    train_folders = viscosity_df[
+        viscosity_df["viscosity"].isin(train_viscosities["viscosity"])
+    ]["folder_name"].tolist()
+    val_folders = viscosity_df[
+        viscosity_df["viscosity"].isin(val_viscosities["viscosity"])
+    ]["folder_name"].tolist()
+    test_folders = viscosity_df[
+        viscosity_df["viscosity"].isin(test_viscosities["viscosity"])
+    ]["folder_name"].tolist()
 
     transform_function = transform()
 
     train_set = DeepViscosityDataset(
-        processed_data, train_folders, transform=transform_function
+        processed_data_path, train_folders, transform=transform_function
     )
     val_set = DeepViscosityDataset(
-        processed_data, val_folders, transform=transform_function
+        processed_data_path, val_folders, transform=transform_function
     )
     test_set = DeepViscosityDataset(
-        processed_data, test_folders, transform=transform_function
+        processed_data_path, test_folders, transform=transform_function
     )
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
