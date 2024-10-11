62c5b0969060756e99b992671599e43874089b47
vjhwong <victor.ju.hin.wong@gmail.com>
2024-10-11 15:39:44 +0200

    refactor: add padding transforms and example usage
    Added new padding transforms (LeftPadding, RightPadding, TopPadding, BottomPadding) to the transforms.py file in the preprocessing/utils directory. These transforms allow for padding of tensors in different directions. Also added an example usage in the main function to demonstrate how to apply the LeftPadding transform to an image tensor.

--- deep_viscosity/preprocessing/utils/transforms.py
+++ deep_viscosity/preprocessing/utils/transforms.py
@@ -1,4 +1,46 @@
+import torch
 import torchvision.transforms as transforms
+from PIL import Image
+
+
+class LeftPadding(object):
+    def __init__(self, padding):
+        self.padding = padding
+
+    def __call__(self, tensor):
+        last_columns = tensor[:, :, -self.padding :]
+        remaining_columns = tensor[:, :, : -self.padding]
+        return torch.cat([last_columns, remaining_columns], dim=-1)
+
+
+class RightPadding(object):
+    def __init__(self, padding):
+        self.padding = padding
+
+    def __call__(self, tensor):
+        first_columns = tensor[:, :, : self.padding]
+        remaining_columns = tensor[:, :, self.padding :]
+        return torch.cat([remaining_columns, first_columns], dim=-1)
+
+
+class TopPadding(object):
+    def __init__(self, padding):
+        self.padding = padding
+
+    def __call__(self, tensor):
+        last_rows = tensor[:, -self.padding :, :]
+        remaining_rows = tensor[:, : -self.padding, :]
+        return torch.cat([last_rows, remaining_rows], dim=-2)
+
+
+class BottomPadding(object):
+    def __init__(self, padding):
+        self.padding = padding
+
+    def __call__(self, tensor):
+        first_rows = tensor[:, : self.padding, :]
+        remaining_rows = tensor[:, self.padding :, :]
+        return torch.cat([remaining_rows, first_rows], dim=-2)
 
 
 def transform() -> transforms.Compose:
@@ -10,3 +52,34 @@ def transform() -> transforms.Compose:
     return transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize(mean=[0], std=[1])]
     )
+
+
+def main():
+    import matplotlib.pyplot as plt
+
+    # Load an image
+    image_path = "path_to_your_image.jpg"
+    image = Image.open(image_path)
+
+    # Convert image to tensor
+    transform_to_tensor = transforms.ToTensor()
+    image_tensor = transform_to_tensor(image)
+
+    # Apply LeftPadding transform
+    padding = 10  # Example padding value
+    left_padding_transform = LeftPadding(padding)
+    transformed_tensor = left_padding_transform(image_tensor)
+
+    # Convert tensor back to image for display
+    transform_to_pil = transforms.ToPILImage()
+    transformed_image = transform_to_pil(transformed_tensor)
+
+    # Display the image
+    plt.imshow(transformed_image)
+    plt.axis("off")
+    plt.show()
+    pass
+
+
+if __name__ == "__main__":
+    main()
