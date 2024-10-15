74adf77a31404d65616f7f2566d4ce8192d21224
vjhwong <victor.ju.hin.wong@gmail.com>
2024-10-15 13:22:37 +0200

    feat: add model saving capabilities to EarlyStopping

--- deep_viscosity/modelling/utils/early_stopping.py
+++ deep_viscosity/modelling/utils/early_stopping.py
@@ -1,37 +1,50 @@
-class EarlyStopping:
-    def __init__(self, patience=5, verbose=False, delta=0):
-        """
-        EarlyStopping constructor.
+import torch
 
-        Parameters:
+class EarlyStopping:
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pth', trace_func=print):
+        """
+        Args:
             patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
             verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
             delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                           Default: 0
+            path (str): Path to save the model checkpoint.
+                        Default: 'checkpoint.pth'
+            trace_func (function): Function to trace print messages.
+                                   Default: print
         """
         self.patience = patience
         self.verbose = verbose
-        self.delta = delta
         self.counter = 0
         self.best_score = None
         self.early_stop = False
+        self.val_loss_min = float('inf')
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
 
-    def __call__(self, metric):
-        """
-        Call method to check if early stopping criteria are met.
+    def __call__(self, val_loss, model):
 
-        Parameters:
-            metric (float): The monitored metric (e.g., validation loss).
-        """
+        score = -val_loss
+
         if self.best_score is None:
-            self.best_score = metric
-        elif metric < self.best_score + self.delta:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
             self.counter += 1
-            if self.verbose:
-                print(f'Validation metric did not improve. Counter: {self.counter}/{self.patience}')
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
-            self.best_score = metric
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
             self.counter = 0
-            if self.verbose:
-                print(f'Validation metric improved to {metric:.6f}. Resetting counter.')
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decreases.'''
+        if self.verbose:
+            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
