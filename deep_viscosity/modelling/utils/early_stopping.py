02675044e1b897e46d40ceeab82668448e821d63
vjhwong <victor.ju.hin.wong@gmail.com>
2024-10-18 11:11:09 +0200

    chore: fix path save

--- deep_viscosity/modelling/utils/early_stopping.py
+++ deep_viscosity/modelling/utils/early_stopping.py
@@ -31,7 +31,7 @@ class EarlyStopping:
 
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, epoch)
+            self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
             self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -39,13 +39,12 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, epoch)
+            self.save_checkpoint(val_loss, model)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model, epoch):
+    def save_checkpoint(self, val_loss, model):
         '''Saves model when validation loss decreases.'''
         if self.verbose:
             self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        basename, extension = self.path.split(".")
-        torch.save(model.state_dict(), f"{basename}_epoch_{epoch}.{extension}")
+        torch.save(model.state_dict(), self.path)
         self.val_loss_min = val_loss
